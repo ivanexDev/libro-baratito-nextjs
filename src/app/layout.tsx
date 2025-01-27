03fdf7580a179a2ae55cad2f128deace6297f8e5
MicaheDev <webdev.michell@gmail.com>
2025-01-27 18:35:32 -0400

    add auth group routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body
         className={`${InterSans.variable} antialiased`}
       >
