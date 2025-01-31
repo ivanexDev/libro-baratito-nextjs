cafe70e33072a617bc37d19f4e7e025a6a99fc03
ivanexDev <ivanexdev@gmail.com>
2025-01-30 22:26:03 -0300

    Add user books page

---
+++ src/components/PlusButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Plus } from 'lucide-react'
+const PlusButton = () => {
+  return (
+    <button className='flex  justify-center items-center  text-white rounded-md bg-orange-primary h-[30px] w-[30px]'>
+        <Plus/>
+    </button>
+  )
+}
+
+export default PlusButton
