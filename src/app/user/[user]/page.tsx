2561925e45a2567fc028fd05873de4cf8de29017
ivanexDev <ivanexdev@gmail.com>
2025-02-05 20:24:04 -0300

    change to real db the books rendered in the user page

--- src/app/user/[user]/page.tsx
+++ src/app/user/[user]/page.tsx
@@ -4,11 +4,11 @@ import { data } from "@/db/db";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ user: string }>;
-}) {
+interface UserPageProps{
+  params: Promise<{ user: string }>
+}
+
+export default async function Page({params,}: UserPageProps) {
   const supabase = await createClient();
   const userName = (await params).user;
 
@@ -24,6 +24,40 @@ export default async function Page({
     return redirect(`/user/${user.email}`);
   }
 
+  const { data: userBooks, error: userBooksError } = await supabase
+  .from("user_books")
+  .select("book_id")
+  .eq("user_id", user.id);
+
+  const userBooksIds = userBooks?.map(book=> book.book_id)
+
+  const { data: allBooksData, error: booksError } = await supabase
+  .from("books")
+  .select("id, book_title, url, image_url, author")
+  .in("id", userBooksIds as string[]);
+
+  // console.log("los libros asociados son:",allBooksData)
+
+  const latestPrices = {};
+
+  for (const bookId of userBooksIds!) {
+    const { data: latestPrice, error: latestPriceError } = await supabase
+      .from("book_prices")
+      .select("price")
+      .eq("book_id", bookId)
+      .order("date", { ascending: false }) // Ordenamos por fecha descendente
+      .limit(1); // Solo queremos el último precio
+
+    if (latestPriceError) throw latestPriceError;
+
+    latestPrices[bookId] = latestPrice.length ? latestPrice[0].price : null;
+  }
+
+  const completeBooks = Object.values(latestPrices).forEach((price, index)=>{
+    allBooksData[index]["price"] = price
+  })
+
+console.log("ultimos precios",allBooksData)
   const books = data.find((user) => user.user === decodedName)?.books || [];
 
   return (
@@ -35,8 +69,8 @@ export default async function Page({
             <h2 className="font-bold text-4xl mb-9">Mis Libros</h2>
             <PlusButton />
           </div>
-          <div className="flex gap-3">
-            {books.map((book, index) => {
+          <div className="flex gap-3 flex-wrap">
+            {allBooksData && allBooksData.map((book, index) => {
               return <BookCard key={index} {...book} />;
             })}
           </div>
--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -3,19 +3,19 @@ import Image from 'next/image'
 import { toCurrency } from '@/utils/toCurrency'
 
 interface BookCardProps {
-    name:string
-    author: string
-    image: string
-    currrentPrice:number
+    book_title: string,
+    image_url: string,
+    author: string,
+    price:number
 }
-const BookCard = ({name, author, image, currrentPrice} :BookCardProps) => {
+const BookCard = ({ book_title, image_url, author, price} :BookCardProps) => {
     return (
     <div className='w-64'>
-        <Image src={image} alt={name} height={300} width={243} className='object-contain h-[300px] w-[240px]'/>
+        <Image src={image_url} alt={book_title} height={300} width={243} className='object-contain h-[300px] w-[240px]'/>
         <div className='m-2'>
-            <h3 className='font-bold text-sm h-11'>{name}</h3>
+            <h3 className='font-bold text-sm h-11'>{book_title}</h3>
             <p>{author}</p>
-            <p className='text-orange-primary font-bold text-xl'>{toCurrency(currrentPrice)}</p>
+            <p className='text-orange-primary font-bold text-xl'>{toCurrency(price)}</p>
         </div>
     </div>
   )
